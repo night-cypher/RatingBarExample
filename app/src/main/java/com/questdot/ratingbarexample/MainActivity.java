9e6be6ada12df4339392539603e020de75db6e56
HP <krist.ng@corous360.com>
2016-10-04 22:20:32 +0800

    update

--- app/src/main/java/com/questdot/ratingbarexample/MainActivity.java
+++ app/src/main/java/com/questdot/ratingbarexample/MainActivity.java
@@ -1,28 +1,75 @@
 package com.questdot.ratingbarexample;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView text_v;
-    private RatingBar rating_b;
+    private TextView text;
+    private RatingBar rating;
+    private Button btnPlus,btnMinus,btnSave;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        rating_b = (RatingBar) findViewById(R.id.ratingBar);
-        text_v = (TextView)findViewById(R.id.textView);
+        rating = (RatingBar) findViewById(R.id.ratingBar);
+        text = (TextView)findViewById(R.id.textView);
 
-        rating_b.setOnRatingBarChangeListener(
+        btnMinus = (Button)findViewById(R.id.button);
+        btnPlus = (Button)findViewById(R.id.button2);
+        btnSave = (Button)findViewById(R.id.btnSave);
+
+
+
+        rating.setOnRatingBarChangeListener(
                 new RatingBar.OnRatingBarChangeListener() {
                     @Override
                     public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
-                        text_v.setText(String.valueOf(rating));
+                        text.setText(String.valueOf(rating));
                     }
                 }
         );
+
+        btnMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                float rate =rating.getRating();
+
+                rating.setRating(rate-1);
+            }
+        });
+
+        btnPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                float rate =rating.getRating();
+
+                rating.setRating(rate+1);
+            }
+        });
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor editor = getSharedPreferences("myrate", MODE_PRIVATE).edit();
+                editor.putFloat("rating", rating.getRating());
+                editor.commit();
+            }
+        });
+
+
+
+
+        SharedPreferences prefs = getSharedPreferences("myrate", MODE_PRIVATE);
+        if (prefs != null) {
+            Float savedrate = prefs.getFloat("rating", 0f);
+            rating.setRating(savedrate);
+
+        }
+
     }
 
 
